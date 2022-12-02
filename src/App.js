71dbe9980f8157c6c0b5f30af4a32a2bf6236f3e
Natasha <natasha.pinder00@gmail.com>
2022-12-02 17:06:40 +0100

    added h2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import './App.css';
 import Carousel from "./Carousel"
 import Navv from "./Navv";
+import Button from 'react-bootstrap/Button';
+
 
 
 
@@ -18,11 +20,12 @@ function App() {
      <Navv />
 
      <h1> "You can check in but you can never check out" </h1>
-      
+     <div className="button">
+       <Button variant="light">Get In Touch</Button>{' '}
     </div>  
- 
+ </div>
 
-   
+   <h2>More Than a Concierge Service</h2>
 
       <header className="App-header">
       
