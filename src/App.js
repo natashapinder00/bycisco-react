5bf0e659852b554c90c69a1be4706fd8fa1cc173
Natasha <natasha.pinder00@gmail.com>
2022-12-06 14:11:08 +0100

    rows

--- src/App.js
+++ src/App.js
@@ -65,7 +65,29 @@ So whether you dream of being right on the beach, having spectacular sea views, 
 ByCisco has the latest Yachts in design, technology and comfort in Ibiza. From fast boats for a day of exploring, to Yachts where you can enjoy infinite days of bliss.
 </div> </Col>
       </Row>
-    
+     <Row>
+        
+        
+        <Col> <div className="col-text">TRANSPORT<br/>
+Booking an Ibiza Chauffeur is a must in high season. Let us do the organising for you so you can enjoy a hassle-free vacation.
+<br/>
+RESTAURANTS<br/>
+You might know exactly where you want to dine, if not, let us inspire you. If you are running late or need to re-arrange, we will happyily do that for you.
+<br/>
+YOGA<br/>
+We have a selection of private yoga instructors, hand picked to suit your wants and needs.
+<Col lg={true}>
+  
+NIGHTLIFE<br/>
+DC10, Hi, Ushuaia, Pacha, Amnesia and Destino, you name it. We will organise your entire night for the ultimate VIP experience.
+<br/>
+CHEFS<br/>
+Our first-class chefs use the best suppliers on the island and aim to meet you every want and need for your event, no matter how big or small.
+<br/>
+NANNIES<br/>
+All nannies have top qualifications paired with the utmost dedication to the children they care for at all times.
+</Col></div> </Col><Col lg={true}><Carousel3 /></Col>
+      </Row>
       
     </div>
   );
