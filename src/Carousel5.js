5697141a743ee84dbce31014623bc91b8d90a37d
Natasha <natasha.pinder00@gmail.com>
2022-12-19 14:09:43 +0100

    beach

--- src/Carousel5.js
+++ src/Carousel5.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel';
-
+import beach from "./beach.jpg";
 import Yacht3 from "./images/yacht3.png";
 import Experience2 from "./images/experience2.png";
 import Experience3 from "./images/experience3.png";
@@ -25,7 +25,7 @@ function UncontrolledExample() {
         
         <img
           className="d-block w-100"
-          src={Experience2}  
+          src={beach}  
           alt="Second slide"
         />
       </Carousel.Item>
