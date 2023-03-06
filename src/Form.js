afbe2f6dd30475de02949c8a01bd364976886328
Natasha Pinder <77281031+natashapinder00@users.noreply.github.com>
2023-03-06 13:17:10 +0100

    changed the form to include divs

--- src/Form.js
+++ src/Form.js
@@ -3,6 +3,56 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 
+
+
+export default function Home() {
+  return <div>
+
+    <form 
+      name="contact v2"
+      method="post"
+      data-netlify="true"
+      onSubmit="submit"
+      data-netlify-honeypot="bot-field"
+    >
+      <input type="hidden" name="form-name" value="contact v2" />
+
+      <div hidden>
+        <input name="bot-field" />
+      </div>
+
+      <div>
+        <label>First name<br />
+            <input type="text" name="first-name" />
+        </label>
+      </div>
+
+      <div>
+        <label>Last name<br />
+            <input type="text" name="last-name" />
+        </label>
+      </div>
+
+      <div>
+        <label htmlFor="email" >Email</label><br />
+        <input id="email" type="email" name="email" />
+      </div>
+
+      <div>
+        <label>Any Comments?<br />
+          <textarea name="comments"></textarea>
+        </label>
+      </div>
+
+      <button type="submit">Submit The Results</button>
+
+    </form>
+
+  </div>
+}
+
+
+/*
 export default function Home() {
   return <div>
 
@@ -50,3 +100,4 @@ export default function Home() {
   </div>
 }
 
+*/
