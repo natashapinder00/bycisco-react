5ce04cd447b667e7ebb1a882c40b42354593a15e
Natasha Pinder <77281031+natashapinder00@users.noreply.github.com>
2023-03-06 13:21:41 +0100

    changed form back to bootstrap

--- src/Form.js
+++ src/Form.js
@@ -8,54 +8,6 @@ import Form from 'react-bootstrap/Form';
 export default function Home() {
   return <div>
 
-    <form 
-      name="contact v2"
-      method="post"
-      data-netlify="true"
-      onSubmit="submit"
-      data-netlify-honeypot="bot-field"
-    >
-      <input type="hidden" name="form-name" value="contact v2" />
-
-      <div hidden>
-        <input name="bot-field" />
-      </div>
-
-      <div>
-        <label>First name<br />
-            <input type="text" name="first-name" />
-        </label>
-      </div>
-
-      <div>
-        <label>Last name<br />
-            <input type="text" name="last-name" />
-        </label>
-      </div>
-
-      <div>
-        <label htmlFor="email" >Email</label><br />
-        <input id="email" type="email" name="email" />
-      </div>
-
-      <div>
-        <label>Any Comments?<br />
-          <textarea name="comments"></textarea>
-        </label>
-      </div>
-
-      <button type="submit">Submit The Results</button>
-
-    </form>
-
-  </div>
-}
-
-
-/*
-export default function Home() {
-  return <div>
-
 
     <Form name="contact v2"
       method="post"
@@ -100,4 +52,3 @@ export default function Home() {
   </div>
 }
 
-*/
